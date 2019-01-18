3b44437ccb010b73d75d0d841e0e196077e1fc73
kupharmd05 <kupharmd05@gmail.com>
2019-01-17 21:25:05 -0600

    fixed dropdown to replace with user choice. Zomato api will now get CityID

--- assets/main.js
+++ assets/main.js
@@ -4,89 +4,112 @@ $(document).ready(function () {
     $("#outgoingDate").datepicker({
         dateFormat: 'yy-mm-dd'
     });
-    
+
     $("#returnDate").datepicker({
         dateFormat: 'yy-mm-dd'
     });
 
+    $("#dropdown-1").on("click", "a", function () {
+        $(this).parents(".dropdown").find(".btn").html($(this).text());
+        $(this).parents(".dropdown").find(".btn").val($(this).data("value"));
 
-    function getDestination() {
+    })
+
+    $("#dropdown-2").on("click", "a", function () {
+        $(this).parents(".dropdown").find(".btn").html($(this).text());
+        $(this).parents(".dropdown").find(".btn").val($(this).data("value"));
+
+    })
+
+    // function getDestination() {
+    //     var destCity = $("#userDestination").val().trim();
+
+    //     console.log(destCity)
+
+    //     var location = $("#location").val().trim();
+    //     var outgoingDate = $("#outgoingDate").val().trim();
+
+    //     var returnDate = $("#returnDate").val().trim();
+
+    //     var bags = $("#bags").val().trim();
+
+    //     var adults = $("#adults").val().trim();
+
+    //     $.ajax({
+    //         url: "https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=" + location + "&destination1=" + destCity + "&departdate1=" + outgoingDate + "&cabin=e&currency=USD&adults="+adults+"&bags="+bags+"&departdate2=" + returnDate + "",
+    //         method: "GET",
+    //         headers: {
+    //             "X-RapidAPI-Key": "95faa2613dmsh9cf38f16b3fd33bp1f9e0djsnbdbe12d99b9f"
+    //         },
+
+    //         // origin1: location,
+    //         // destination1: destCity,
+    //         // departuredate1: outgoingDate,
+    //         // departuredate2: returnDate,
+    //         // cabin: "e",
+    //         // currency: "USD",
+    //         // adults: adults,
+    //         // bags: bags,
+
+    //     }).then(function (response) {
+    //         console.log(response)
+
+
+
+    //     });
+
+    // }
+    // //Zomato api
+    function getFood() {
         var destCity = $("#userDestination").val().trim();
 
-
-        
         console.log(destCity)
-        var location = $("#location").val().trim();
-        var outgoingDate = $("#outgoingDate").val().trim();
 
-        var returnDate = $("#returnDate").val().trim();
 
-        // var bags = $("bags").val().trim();
-
-        // var adults = $("#adults").val().trim();
+        const cityurlQuery = "https://developers.zomato.com/api/v2.1/cities?q=" + destCity;
 
         $.ajax({
-            url: "https://apidojo-kayak-v1.p.rapidapi.com/flights/create-session?origin1=" + location + "&destination1=" + destCity + "&departdate1=" + outgoingDate + "&cabin=e&currency=USD&adults=1&bags=0&departdate2=" + returnDate + "",
-            method: "GET",
-            headers: {
-                "X-RapidAPI-Key": "95faa2613dmsh9cf38f16b3fd33bp1f9e0djsnbdbe12d99b9f"
-            },
-
-            // origin1: location,
-            // destination1: destCity,
-            // departuredate1: outgoingDate,
-            // departuredate2: returnDate,
-            // cabin: "e",
-            // currency: "USD",
-            // adults: adults,
-            // bags: bags,
-
-        }).then(function (response) {
-            console.log(response)
-
-
-
-        });
-
-    }
-    //Zomato api
-    function getFood() {
-        var userInput = $("#userDestination").val().trim();
-        let destCity = userInput
-        console.log(userInput)
-        let radius = 2000;
-        let no_of_resturants = 20
-
-        let urlQuery = "https://developers.zomato.com/api/v2.1/search?entity_type=city&q=" + destCity
-            + "&start=01&count=" + no_of_resturants + "&radius=" + radius + "M&sort=rating"
-
-        $.ajax({
-            url: urlQuery,
+            url: cityurlQuery,
             method: "GET",
             headers: {
                 "user-key": "b485d5465e4552f6c7357bacb40808dc"
+            },
+            success: function () {
+                getRestaurants();
             }
         }).then(function (response) {
-            console.log(response.restaurants)
-            let len = response.restaurants.length
-            for (let i = 0; i < len; i++) {
+            console.log(response)
+            var cityInfo = response
 
-                let result = response.restaurants
-                console.log(result[i].restaurant)
-                console.log("name : " + result[i].restaurant.name)
-                console.log("cuisines : " + result[i].restaurant.cuisines)
-                console.log("Location : " + result[i].restaurant.location.address)
-                console.log("Rating : " + result[i].restaurant.user_rating.aggregate_rating)
-                console.log("Image_url : " + result[i].restaurant.photos_url)
+            let cityID = 0
+
+            if (cityInfo !== "") {
+
+                let cityID = cityInfo.location_suggestions[0].id;
+
+                console.log(cityID);
             }
+            function getRestaurants(){
+            $.ajax({
+                url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&sort=rating",
+                method: "GET",
+                headers: {
+                    "user-key": "b485d5465e4552f6c7357bacb40808dc"
+                }
+            }).then(function (response) {
+                console.log(response);
+                });
+
+            }   
+
         });
-    }
+    };
 
     $("#submit").on("click", event => {
         event.preventDefault()
-        getFood(),
-            getDestination()
-    })
-
+        getFood()
+    // getDestination()
+    });
 
 });
+
